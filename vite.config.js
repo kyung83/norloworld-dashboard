989606f1d044073dd73075b1cfe15c452c703250
kyung83 <161100189+kyung83@users.noreply.github.com>
2025-09-02 16:23:16 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/norloworld-dashboard/"
+  base: "/norloworld-dashboard/", // <-- Make sure this matches your repo name exactly
 })
+
